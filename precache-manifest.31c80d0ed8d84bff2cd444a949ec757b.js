d592608f477630adfe05f0ed87d3f4fdd1bccff7
Rajesh Pokharkar <rajeshpokharkar81@gmail.com>
2024-07-29 20:47:57 +0530

    Updates

---
+++ precache-manifest.31c80d0ed8d84bff2cd444a949ec757b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cc0067ea02448bef223b213fef79fe",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "9f41178cc0aa61b1f396",
+    "url": "/react-invoice-generator/static/css/2.fbdd25f2.chunk.css"
+  },
+  {
+    "revision": "5ba2ad488b855b35d37e",
+    "url": "/react-invoice-generator/static/css/main.6de426f8.chunk.css"
+  },
+  {
+    "revision": "9f41178cc0aa61b1f396",
+    "url": "/react-invoice-generator/static/js/2.044f6526.chunk.js"
+  },
+  {
+    "revision": "27eb1d1e8906a6c757af172b78c289d1",
+    "url": "/react-invoice-generator/static/js/2.044f6526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba2ad488b855b35d37e",
+    "url": "/react-invoice-generator/static/js/main.fe8d9a79.chunk.js"
+  },
+  {
+    "revision": "08d117e8c1ae5e840a5b",
+    "url": "/react-invoice-generator/static/js/runtime-main.e64e8034.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "e631f0ca65031b20fe23668e166aa85b",
+    "url": "/react-invoice-generator/static/media/remove.e631f0ca.svg"
+  },
+  {
+    "revision": "a700735566a3701072afbd164b80e690",
+    "url": "/react-invoice-generator/static/media/resize.a7007355.svg"
+  }
+]);
